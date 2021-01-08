2ffa2ee9004ff0ba573cdf654888d619b7c39a60
Ar-Kareem <abdulrahman_3000@hotmail.com>
2021-01-08 18:53:46 +0300

    Enable passing logger to MyHTTPHandler

--- src/utils/simple_server.py
+++ src/utils/simple_server.py
@@ -1,16 +1,27 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import threading
 import socket
+from logging import Logger
 
 
 class MyHTTPHandler(BaseHTTPRequestHandler):
+    """
+    This class can be used with an HTTPServer as if it is a BaseHTTPRequestHandler
+    The difference is that an HTTPServer creates a new instance of BaseHTTPRequestHandler for every request
+    which means that no variables can persist throughout the request handler instance.
+    This class helps with that by allowing it to be initialized then can store instance variables,
+    and each time httpserver attempts to initialize the request handler (__call__ will be invoked).
+    This only works because super().__init__ is safe to be invoked multiple times.
+    This can be thought of as a metaclass except it's not.
+    """
     # noinspection PyMissingConstructor
-    def __init__(self):
+    def __init__(self, logger: Logger = None):
         # do not call super yet.
-        pass
+        self.logger = logger
 
     def __call__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        return self
 
     def bind(self, func, as_name: str = None) -> None:
         """
@@ -22,6 +33,10 @@ class MyHTTPHandler(BaseHTTPRequestHandler):
             as_name = func.__name__
         bound_method = func.__get__(self, self.__class__)
         setattr(self, as_name, bound_method)
+
+    def log_message(self, format_: str, *args: any) -> None:
+        if self.logger is not None:
+            self.logger.info(format_, *args)
 
 
 def start_server(handler: MyHTTPHandler, server_port: int = 8049, localhost: bool = False) -> HTTPServer:
