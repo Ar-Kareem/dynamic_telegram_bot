01a90e6d9c482d43a5c2e1be8802a2553cc6fd03
Ar-Kareem <abdulrahman_3000@hotmail.com>
2021-01-08 14:41:49 +0300

    Move init logger code to start.py instead of main.py (main.py only calls start.py)

--- src/core/start.py
+++ src/core/start.py
@@ -1,3 +1,4 @@
+import sys
 from threading import Thread
 import logging
 
@@ -9,10 +10,26 @@ logger = logging.getLogger(__name__)
 
 
 def start():
-    logger.info('Starting...')
+    init_logger()
     pocket = Pocket()
     init_scripts(pocket)
     reducer_loop(pocket)
+
+
+def init_logger():
+    warn_file_handler = logging.FileHandler(filename='logs/logs_warn.log')
+    warn_file_handler.setLevel(logging.WARN)
+    debug_file_handler = logging.FileHandler(filename='logs/logs_debug.log')
+    debug_file_handler.setLevel(logging.DEBUG)
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.setLevel(logging.DEBUG)
+    handlers = [stderr_handler, debug_file_handler, warn_file_handler]
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format='[%(asctime)s] {%(filename)s:%(lineno)d - %(name)s} %(levelname)s - %(message)s',
+        handlers=handlers
+    )
 
 
 def init_scripts(pocket: Pocket):
