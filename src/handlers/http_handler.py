4cb5a643f0ae3fe54cd2f46908fab682dd662441
Ar-Kareem <abdulrahman_3000@hotmail.com>
2021-01-08 18:58:30 +0300

    Enable exception handling and logging in http_handler.py

--- src/handlers/http_handler.py
+++ src/handlers/http_handler.py
@@ -11,36 +11,46 @@ pocket_dict_name = 'http_handler_dict'
 
 def init(pocket: Pocket):
     pocket.set(pocket_dict_name, {})
-    handler = MyHTTPHandler()
+
+    # create HTTP Handler and bind functionality to it
+    handler = MyHTTPHandler(logger=logger)
     handler.pocket = pocket
     for func in (do_POST, do_GET):
         handler.bind(func)
-    http_server = start_server(handler, server_port=int(pocket.config['SERVER']['port']))
 
+    # Attempt to start server at port given in config file
+    server_port = int(pocket.config['SERVER']['port'])
+    try:
+        http_server = start_server(handler, server_port=server_port)
+    except Exception:
+        logger.exception('Failed to start HTTP server at port %d', server_port)
+
+    # Register Handlers for adding functionality to the server and terminating the server
     pocket.reducer.register_handler(trigger=Terminate, callback=lambda _: close_server(http_server))
     pocket.reducer.register_handler(trigger=AddServerHandler, callback=partial(add_server_handler, pocket=pocket))
 
 
 def add_server_handler(action: AddServerHandler, pocket: Pocket):
-    module_inner_pocket: dict = pocket.get(pocket_dict_name)
+    """This is called when an AddServerHandler action is dispatched and this function will add a new HTTP Handler"""
+    module_dict: dict = pocket.get(pocket_dict_name)
     method = action.method.upper()
     if method not in ('POST', 'GET'):
         logger.error('UNKNOWN METHOD: %s', method)
         return
-    for other_prefix, _ in module_inner_pocket.setdefault(method, []):
+    for other_prefix, _ in module_dict.setdefault(method, []):
         if action.prefix_to_handle.startswith(other_prefix) or other_prefix.startswith(action.prefix_to_handle):
             logger.error('Attempted to register server handler with a conflicting prefix old:(%s) new:(%s)',
                          other_prefix, action.prefix_to_handle)
             return
     t = (action.prefix_to_handle, action.handler)
-    module_inner_pocket.setdefault(method, []).append(t)
+    module_dict.setdefault(method, []).append(t)
 
 
 # Methods to be bound to MyHTTPHandler
 
 def call_appropriate_handler(self: MyHTTPHandler, method: str):
-    module_inner_pocket: dict = self.pocket.get(pocket_dict_name)
-    handlers = module_inner_pocket.get(method)
+    module_dict: dict = self.pocket.get(pocket_dict_name)
+    handlers = module_dict.get(method)
     if handlers is None:
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
@@ -48,7 +58,10 @@ def call_appropriate_handler(self: MyHTTPHandler, method: str):
         return
     for prefix, handler in handlers:
         if self.path.startswith(prefix):
-            handler(self)
+            try:
+                handler(self)
+            except Exception:
+                logger.exception('Exception when handling HTTP request')
             return
 
 
