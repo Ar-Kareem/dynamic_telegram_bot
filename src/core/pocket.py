a1e02d70c199b04c38b93dc8c0b405f2484db601
Ar-Kareem <abdulrahman_3000@hotmail.com>
2021-01-08 16:21:37 +0300

    add start time to pocket

--- src/core/pocket.py
+++ src/core/pocket.py
@@ -1,3 +1,4 @@
+import time
 from threading import Thread
 
 from src.utils import utils
@@ -9,8 +10,12 @@ class Pocket:
         self.store = Store()
         self.reducer = Reducer(store=self.store)
         self.config = utils.init_config()
+
         self.active_threads: list[Thread] = []
         self.inner_pocket: dict[str, any] = {}
+
+        self.start_time = time.localtime()
+        self.start_time_str = time.strftime("%Y-%m-%d %H:%M:%S", self.start_time)
         self.telegram_updater = None
 
     def get(self, name: str) -> any:
