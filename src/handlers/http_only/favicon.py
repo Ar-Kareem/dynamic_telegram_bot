d9931f4f84483e0ef54e4c409f8f5bcdea54bb7f
Ar-Kareem <abdulrahman_3000@hotmail.com>
2021-06-09 22:48:10 +0300

    update favicon to cache instead of open and read file every time

--- src/handlers/http_only/favicon.py
+++ src/handlers/http_only/favicon.py
@@ -6,6 +6,7 @@ from src.utils.simple_server.simple_server import MyHTTPHandler
 
 
 def init(pocket: Pocket):
+    pocket.set(__file__, {})
     pocket.store.dispatch(AddServerHandler('get', '/favicon.ico/', serve_favicon))
 
 
@@ -13,5 +14,14 @@ def serve_favicon(self: MyHTTPHandler):
     self.send_response(200)
     self.send_header('Content-type', 'image/png')
     self.end_headers()
-    with open(self.pocket.database_dir / 'misc' / 'favicon.ico', 'rb') as f:
-        self.wfile.write(f.read())
+    d: dict = self.pocket.get(__file__)
+    if 'favicon.ico' in d:
+        self.wfile.write(d['favicon.ico'])
+        return
+    favicon_path = Path(self.pocket.database_dir / 'misc' / 'favicon.ico')
+    if not favicon_path.exists():
+        d['favicon.ico'] = b''
+        return
+    with open(favicon_path, 'rb') as f:
+        d['favicon.ico'] = f.read()
+        self.wfile.write(d['favicon.ico'])
