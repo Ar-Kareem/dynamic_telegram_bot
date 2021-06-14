bd0a8f1bce14eb143b4d70bb74668f0b8980f99d
Ar-Kareem <abdulrahman_3000@hotmail.com>
2021-06-15 00:08:19 +0300

    database.py util - implement logging

--- src/utils/database.py
+++ src/utils/database.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from pathlib import Path
 from shutil import rmtree
@@ -6,24 +7,27 @@ from datetime import datetime
 import pickle
 import secrets
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM  # suggested by stackoverflow.com/questions/36117046/
+from typing import List
 
-
+logger = logging.getLogger(__name__)
 _METADATA_FILENAME = '.metadata.ini'
 
 
-def compress(dir_path, output_file, encrypt_key, check_callback=None):
+def compress(dir_path, output_file, encrypt_key, check_callback=None, verbose=False):
     result = {
         'name': dir_path.name,
         'folders': [],
         'date': datetime.today().strftime('%Y-%m-%d-%H:%M:%S'),
     }
-    dirs = [dir_path / n for n in os.listdir(dir_path) if (dir_path / n).is_dir()]
+    dirs: List[Path] = [dir_path / n for n in os.listdir(dir_path) if (dir_path / n).is_dir()]
     for d in dirs:
         if check_callback is not None:
             config = ConfigParser()
             config.read(d / _METADATA_FILENAME)
             if not check_callback(d, config):  # callback returned false, do not compress
                 continue
+        if verbose:
+            logger.info('collecting dir: %s', d.name)
         result['folders'].append(_collect_dir(d))
 
     # pickle dictionary, use protocol 4 which was added in python 3.4 (5 only added in 3.8 so not using it)
@@ -91,7 +95,7 @@ def _expand_collected_dir(data: dict, cur_path: Path):
     for file_data in data['files']:
         file_path = cur_folder / file_data['name']
         if file_path.exists():
-            print('file already exists...', file_path)
+            logger.info('file already exists...', file_path)
             continue
         with open(file_path, 'wb') as f:
             f.write(file_data['data'])
@@ -99,7 +103,7 @@ def _expand_collected_dir(data: dict, cur_path: Path):
 
 def _print_random_key(length=32):
     k = secrets.token_bytes(length)
-    print("".join('\\x%02x' % b for b in k))
+    logger.info("".join('\\x%02x' % b for b in k))
 
 
 def string_to_key(_key):
@@ -110,8 +114,8 @@ def date_delete_callback(fldr_path: Path, fldr_config: ConfigParser, db_date: da
     """Helper function implementing a delete callback that compares dates"""
     parsed = fldr_config.get('METADATA', 'restored_from', fallback=None)
     if parsed is None:
-        print('cannot determine if db copy is newer or older since .ini file does not contain date')
+        logger.info('cannot determine if db copy is newer or older since .ini file does not contain date')
         return False
     fldr_date = datetime.strptime(parsed, '%Y-%m-%d-%H:%M:%S')
-    # print(db_date > fldr_date, fldr_path.name, fldr_date, db_date)
+    # logger.info(db_date > fldr_date, fldr_path.name, fldr_date, db_date)
     return db_date > fldr_date
