fedeb9be9f8c66569146aca0be4b50facc5f21cd
Ar-Kareem <abdulrahman_3000@hotmail.com>
2021-06-30 00:20:38 +0300

    implement separate log files to separate module logs and current project logs

--- main.py
+++ main.py
@@ -1,9 +1,48 @@
 #!/usr/bin/env python3
 
+import sys
+from pathlib import Path
+import logging
 import argparse
+
 from src.core import start
 
+
+def init_logger():
+    formatter = logging.Formatter('[%(asctime)s] {%(filename)s:%(lineno)d - %(name)s} %(levelname)s - %(message)s')
+    Path("logs").mkdir(exist_ok=True)
+
+    # set up log for this project (warn/debug/console)
+    warn_file_handler = logging.FileHandler(filename='logs/logs_warn.log')
+    warn_file_handler.setLevel(logging.WARN)
+    warn_file_handler.setFormatter(formatter)
+    debug_file_handler = logging.FileHandler(filename='logs/logs_debug.log')
+    debug_file_handler.setLevel(logging.DEBUG)
+    debug_file_handler.setFormatter(formatter)
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.setLevel(logging.DEBUG)
+    stderr_handler.setFormatter(formatter)
+    # add handlers to the logger
+    my_root = logging.getLogger('src')
+    my_root.setLevel(logging.DEBUG)
+    my_root.propagate = False
+    my_root.addHandler(warn_file_handler)
+    my_root.addHandler(debug_file_handler)
+    my_root.addHandler(stderr_handler)
+
+    # set up log for other modules (warn/console)
+    warn_file_handler = logging.FileHandler(filename='logs/logs_modules_warn.log')
+    warn_file_handler.setLevel(logging.WARN)
+    warn_file_handler.setFormatter(formatter)
+    # add handlers to the logger
+    root = logging.getLogger('')
+    root.setLevel(logging.INFO)
+    root.addHandler(warn_file_handler)
+    root.addHandler(stderr_handler)
+
+
 if __name__ == '__main__':
+    init_logger()
     parser = argparse.ArgumentParser()
     parser.add_argument("--settings", help="Custom settings.ini file")
     config_path = parser.parse_args().settings
--- src/core/start.py
+++ src/core/start.py
@@ -1,5 +1,3 @@
-import sys
-from pathlib import Path
 from threading import Thread
 import logging
 
@@ -11,30 +9,9 @@ logger = logging.getLogger(__name__)
 
 
 def start(config_path=None):
-    init_logger()
     pocket = Pocket(config_path=config_path)
     init_scripts(pocket)
     reducer_loop(pocket, config_path)
-
-
-def init_logger():
-    Path("logs").mkdir(exist_ok=True)
-    warn_file_handler = logging.FileHandler(filename='logs/logs_warn.log')
-    warn_file_handler.setLevel(logging.WARN)
-    debug_file_handler = logging.FileHandler(filename='logs/logs_debug.log')
-    debug_file_handler.setLevel(logging.DEBUG)
-    stderr_handler = logging.StreamHandler(sys.stderr)
-    stderr_handler.setLevel(logging.DEBUG)
-    handlers = [stderr_handler, debug_file_handler, warn_file_handler]
-
-    my_root = logging.getLogger('src')
-    my_root.setLevel(logging.DEBUG)
-
-    logging.basicConfig(
-        level=logging.INFO,
-        format='[%(asctime)s] {%(filename)s:%(lineno)d - %(name)s} %(levelname)s - %(message)s',
-        handlers=handlers
-    )
 
 
 def init_scripts(pocket: Pocket):
